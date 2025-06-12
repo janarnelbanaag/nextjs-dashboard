e0c16f144f6a71b6bd7bd39c6cd8dd3772d7a69b
janarnelbanaag <janarnelbanaag@gmail.com>
2025-06-12 23:36:22 +0800

    route group

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,13 @@
-import { Card } from "../ui/dashboard/cards";
-import RevenueChart from "../ui/dashboard/revenue-chart";
-import LatestInvoices from "../ui/dashboard/latest-invoices";
-import { lusitana } from "../ui/fonts";
+import { Card } from "../../ui/dashboard/cards";
+import RevenueChart from "../../ui/dashboard/revenue-chart";
+import LatestInvoices from "../../ui/dashboard/latest-invoices";
+import { lusitana } from "../../ui/fonts";
 
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
+import {
+	fetchRevenue,
+	fetchLatestInvoices,
+	fetchCardData,
+} from "../../lib/data";
 
 const Page = async () => {
 	const revenue = await fetchRevenue();
