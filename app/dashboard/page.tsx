a47070f2c035dd613a418c271484426ee3af15a3
janarnelbanaag <janarnelbanaag@gmail.com>
2025-06-12 18:55:53 +0800

    revenue chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,11 @@ import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import { lusitana } from "../ui/fonts";
 
+import { fetchRevenue } from "../lib/data";
+
 const Page = async () => {
+	const revenue = await fetchRevenue();
+
 	return (
 		<main>
 			<h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -20,7 +24,7 @@ const Page = async () => {
         /> */}
 			</div>
 			<div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-				{/* <RevenueChart revenue={revenue}  /> */}
+				<RevenueChart revenue={revenue} />
 				{/* <LatestInvoices latestInvoices={latestInvoices} /> */}
 			</div>
 		</main>
