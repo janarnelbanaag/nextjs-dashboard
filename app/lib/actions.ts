3fbb587dc566ce787241384bbc4b82c1e5a7d7f8
janarnelbanaag <janarnelbanaag@gmail.com>
2025-06-17 21:30:19 +0800

    action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,25 @@
 "use server";
 
-export async function createInvoice(formData: FormData) {}
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+export async function createInvoice(formData: FormData) {
+	const { customerId, amount, status } = {
+		customerId: formData.get("customerId"),
+		amount: formData.get("amount"),
+		status: formData.get("status"),
+	};
+	const amountInCents = Number(amount) * 100;
+	const date = new Date().toISOString().split("T")[0];
+
+	await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+	revalidatePath("/dashboard/invoices");
+	redirect("/dashboard/invoices");
+}
