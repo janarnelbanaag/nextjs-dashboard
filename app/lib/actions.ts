5677287959ca38b49859758918c908a0a77a9e97
janarnelbanaag <janarnelbanaag@gmail.com>
2025-06-18 09:55:26 +0800

    handling errors with error.tsx and not-found.tsx

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,11 +15,49 @@ export async function createInvoice(formData: FormData) {
 	const amountInCents = Number(amount) * 100;
 	const date = new Date().toISOString().split("T")[0];
 
-	await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+	try {
+		await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+	} catch (e) {
+		console.error(e);
+	}
 
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+	const { customerId, amount, status } = {
+		customerId: formData.get("customerId"),
+		amount: formData.get("amount"),
+		status: formData.get("status"),
+	};
+	const amountInCents = Number(amount) * 100;
+
+	try {
+		await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+	} catch (e) {
+		console.error(e);
+	}
+
+	revalidatePath("/dashboard/invoices");
+	redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+	throw new Error("Failed to Delete Invoice");
+
+	try {
+		await sql`DELETE FROM invoices WHERE id = ${id}`;
+	} catch (e) {
+		console.error(e);
+	}
+
+	revalidatePath("/dashboard/invoices");
 }
