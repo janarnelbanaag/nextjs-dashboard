5efd32fe415206afbe9b18cd210753aeb1158598
janarnelbanaag <janarnelbanaag@gmail.com>
2025-06-20 17:31:42 +0800

    added authenticate action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,27 @@
 "use server";
 
-import { error } from "console";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod";
+
+export async function authenticate(prevState: State, formData: FormData) {
+	try {
+		await signIn("credentials", formData);
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case "CredentialsSignin":
+					return "Invalid Credentials.";
+				default:
+					return "Something went wrong.";
+			}
+		}
+		throw error;
+	}
+}
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 const FormSchema = z.object({
